b6923f7bbad0abbf59d8dbbec4b0c96a69749b45
danverde <daniel.green@stukent.com>
2020-11-24 15:50:35 -0700

    save file cleanup

--- Shipwreck/Control/ShipwreckController.cs
+++ Shipwreck/Control/ShipwreckController.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
+using Shipwreck.Helpers;
 using Shipwreck.View;
 
 namespace Shipwreck.Control
@@ -10,44 +11,34 @@ namespace Shipwreck.Control
     {
         public static bool TrySaveGame(string fileName)
         {
-            var success = true;
-            var savePath = Shipwreck.Settings.SavePath;
-            
-            
-            // TODO validate name & extension
-            if (!fileName.Contains(".json")) fileName += ".json";
-            
-            
-            // check for existing file & ask to override
             try
             {
-                var existingFileNames = Directory.GetFiles(savePath).Select(Path.GetFileName).ToList();
-                if (existingFileNames.Contains(fileName))
+                if (string.IsNullOrEmpty(fileName)) return false;
+                
+                var savePath = Shipwreck.Settings.SavePath;
+            
+                // validate name & extension
+                fileName = FileWriter.AddExtension(".json", fileName);
+            
+                // check for existing file & ask to override
+                var fileExists = FileWriter.FileExists(savePath, fileName);
+                if (fileExists)
                 {
-                    var overwrite = GameMenuView.OverwriteFileName("fileName");
+                    var overwrite = GameMenuView.OverwriteFileName(fileName);
                     if (!overwrite) return false;
                 }
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                Console.WriteLine($"Unable to open directory {savePath}");
-                return false;
-            }
 
-            
-            var gameString = JsonConvert.SerializeObject(Shipwreck.CurrentGame);
-            var filePath =  Path.Combine(savePath, fileName);
-
-            try
-            {
+                // write the file
+                var gameString = JsonConvert.SerializeObject(Shipwreck.CurrentGame);
+                var filePath =  Path.Combine(savePath, fileName);
                 File.WriteAllText(filePath, gameString);
+                return true;
             }
             catch (Exception ex)
             {
-                success = false;
+                return false;
             }
             
-            return success;
         }
     }
 }
--- Shipwreck/Helpers/FileWriter.cs
+++ Shipwreck/Helpers/FileWriter.cs
@@ -1,4 +1,23 @@
-$HEADER$namespace $NAMESPACE$
+using System.IO;
+using System.Linq;
+using Shipwreck.View;
+
+namespace Shipwreck.Helpers
 {
-  public class $CLASS$ {$END$}
+    public static class FileWriter
+    {
+        public static string AddExtension(string extension, string fileName)
+        {
+            var existingExtension = Path.GetExtension(fileName);
+            if (string.IsNullOrEmpty(existingExtension)) fileName += extension;
+            else if (existingExtension != extension) fileName = fileName.Replace(extension, extension);
+            return fileName;
+        }
+
+        public static bool FileExists(string directory, string fileName)
+        {
+            var existingFileNames = Directory.GetFiles(directory).Select(Path.GetFileName).ToList();
+            return existingFileNames.Contains(fileName);
+        }
+    }
 }
